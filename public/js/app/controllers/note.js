55eacc1710518ff86a32b9d8efe945f2c74bfef9
Nik Butenko <nik@butenko.me>
2013-09-15 16:04:36 +1000

    Add note form, marker creation and marker dragging integrated with add form

--- public/js/app/controllers/note.js
+++ public/js/app/controllers/note.js
@@ -3,10 +3,10 @@
 define(
 	'app/controllers/note',
 	[
-		'dom', 'underscore', 'lib/app',
+		'dom', 'underscore', 'lib/app', 'lib/dom/form', 'lib/messenger',
 		'lib/messenger'
 	],
-	function ($, _, app) {
+	function ($, _, app, form, messenger) {
 
 
 		var actions = {};
@@ -28,15 +28,49 @@ define(
 			}
 		});
 
-		function onAddReady(event) {
+		function onAddViewReady(event) {
 			var $element = $(event.target).closest('.app_controllers_note-add');
 			console.log('app/controllers/note', 'onAddReady', $element);
+		}
 
+		function onAddFormSubmit(event) {
+			event.preventDefault();
+			var $form = $(event.target).closest('.app_controllers_note-add-form');
 
+			try {
+			} catch (error) {
+				return $form.trigger('lib/messenger:show', [messenger.TYPE_ERROR, error.message]);
+			}
+
+			if ($form.find(':invalid').length > 0) {
+				return $form.trigger('lib/messenger:show', [messenger.TYPE_ERROR, 'Please, fill the form']);
+			}
+
+			form.disable($form);
+
+			return setTimeout(function () {
+				form.enable($form);
+				return $form.trigger('lib/messenger:show', [messenger.TYPE_MESSAGE, 'OK']);
+			}, 2000);
+		}
+
+		function onAddFormReset(event) {
+			event.preventDefault();
+			var $form = $(event.target).closest('.app_controllers_note-add-form');
+			$form.trigger('app/map:markerReset');
+		}
+
+		function onMarkerMoved(event, lat, lng) {
+			var $element = app.$root.find('.app_controllers_note-add');
+			$element.find('.app_controllers_note-lat').val(lat);
+			$element.find('.app_controllers_note-lng').val(lng);
 		}
 
 		app.$root
-			.on('lib/layout:renderBlock:done', '.app_controllers_note-add', onAddReady);
+			.on('submit', '.app_controllers_note-add .app_controllers_note-add-form', onAddFormSubmit)
+			.on('reset', '.app_controllers_note-add .app_controllers_note-add-form', onAddFormReset)
+			.on('lib/layout:renderBlock:done', '.app_controllers_note-add', onAddViewReady)
+			.on('app/map:markerMove:done', null, onMarkerMoved);
 
 
 		return actions;
--- public/js/app/map.js
+++ public/js/app/map.js
@@ -6,6 +6,10 @@
 define('app/map', ['dom', 'underscore', 'lib/app', 'lib/map'], function ($, _, app, map) {
 
 
+	var markers = [],
+		temporaryMarker = null;
+
+
 	function markerAdd(googleMap, latLng, text) {
 		var marker = new google.maps.Marker({map: googleMap, position: latLng, draggable: true});
 		var infowindow = new google.maps.InfoWindow({content: text});
@@ -15,55 +19,79 @@ define('app/map', ['dom', 'underscore', 'lib/app', 'lib/map'], function ($, _, a
 		return marker;
 	}
 
-	function onMarkerAdd(event, lat, lng, text) {
 
+	function temporaryMarkerAdd(googleMap, latLng) {
+		if (temporaryMarker === null) {
+			temporaryMarker = new google.maps.Marker({map: googleMap, position: latLng, draggable: true});
+			google.maps.event.addListener(temporaryMarker, 'dragend', function (event) {
+				var latLng = event.latLng;
+				app.$root.trigger('app/map:markerMove:done', [latLng.lat(), latLng.lng()]);
+			});
+			app.$root.trigger('lib/dispatcher:dispatch', ['/note/add']);
+		} else {
+			temporaryMarker.setPosition(latLng);
+			app.$root.trigger('app/map:markerMove:done', [latLng.lat(), latLng.lng()]);
+		}
+		return temporaryMarker;
+	}
+
+
+	function onMarkerAdd(event, lat, lng, text) {
 		map(function (googleMap) {
 			return markerAdd(googleMap, new google.maps.LatLng(lat, lng), text);
 		})
-
 	}
 
-	
-	
-	
-	
-	
-	
 
 	function onMapClick(googleMap) {
-		return function (event) {
-			var latLng = event.latLng;
-			
-			app.$root.trigger('lib/dispatcher:dispatch', ['/note/add']);
-			
-			markerAdd(googleMap, latLng, 'test');
+		return _.debounce(function (event) {
+			return temporaryMarkerAdd(googleMap, event.latLng);
+		}, 300, true);
+	}
+
+
+	function onNoteAddReady(event) {
+		if (!temporaryMarker) {
+			return app.$root.trigger('lib/dispatcher:dispatch', ['/']);
 		}
+		var $element = $(event.target).closest('.app_controllers_note-add'),
+			latLng = temporaryMarker.getPosition();
+		$element.trigger('app/map:markerMove:done', [latLng.lat(), latLng.lng()]);
+		return $element;
 	}
 
+	function onMarkerReset() {
+		if (temporaryMarker) {
+			google.maps.event.clearInstanceListeners(temporaryMarker);
+			temporaryMarker.setMap(null);
+			temporaryMarker = null;
+		}
 
+		return app.$root.trigger('lib/dispatcher:dispatch', ['/']);
+	}
+
+	app.$root
+		.on('app/map:markerAdd', onMarkerAdd)
+		.on('app/map:markerReset', onMarkerReset)
+		.on('lib/layout:renderBlock:done', '.app_controllers_note-add', onNoteAddReady);
+
+
+	/**
+	 * @param {Map} googleMap
+	 */
 	function setup(googleMap) {
-
 		google.maps.event.addListener(googleMap, 'click', onMapClick(googleMap));
-
-//		app.$root.trigger('lib/map:marker-add', [-33.8389324 , 151.2155156 , 'hello']);
-//		google.maps.event.addListener(googleMarker, 'dragend', function () {
-//			var position = googleMarker.getPosition();
-//			$element.trigger('googleMap:markerMoved', [position.lat(), position.lng()]);
-//		});
-//
-//		googleMap.panTo(googleMarker.getPosition());
-
 	}
+
 
 	function init() {
 		app.$root.find('#googleMap').trigger('lib/map:init');
 		map(setup);
 	}
 
+
 	$(init);
 
-
-	app.$root.on('app/map:markerAdd', onMarkerAdd);
 
 	return true;
 });
